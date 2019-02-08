96a1fb97080b6c25fce2bd602e4e2c0b10bada9f
xaviguasch <xaviguasch@gmail.com>
2019-02-08 14:15:39 +0100

    adding EJS templates

--- app.js
+++ app.js
@@ -3,9 +3,45 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-    res.send('Hello')
+    const today = new Date()
+    const currentDay = today.getDay()
+    let day = ""
+
+    switch (currentDay) {
+        case 0:
+            day = 'Sunday'
+            break;
+        case 1:
+            day = 'Monday'
+            break;
+        case 2:
+            day = 'Tuesday'
+            break;
+        case 3:
+            day = 'Wednesday'
+            break;
+        case 4:
+            day = 'Thursday'
+            break;
+        case 5:
+            day = 'Friday'
+            break;
+        case 6:
+            day = 'Saturday'
+            break;
+        default:
+            console.log(`Error: current day is equal to: ${currentDay}`);
+    }
+
+    res.render('list', {
+        kindOfDay: day
+    })
 })
+
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Server is running on port 3000');
