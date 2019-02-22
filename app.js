92f9b064993febc345b3c6b27a01ac7157f4c6d1
xaviguasch <xaviguasch@gmail.com>
2019-02-22 19:25:51 +0100

    playing with ejs layouts

--- app.js
+++ app.js
@@ -54,7 +54,9 @@ app.get('/work', function (req, res) {
     })
 })
 
-
+app.get('/about', function (req, res) {
+    res.render('about')
+})
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Server is running on port 3000');
