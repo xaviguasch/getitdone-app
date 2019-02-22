6246db0166688ba4cc0660d58f3a8ed52631f97c
xaviguasch <xaviguasch@gmail.com>
2019-02-22 16:52:09 +0100

    implemented passing data from webpage to server

--- app.js
+++ app.js
@@ -3,44 +3,40 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+let items = ['buy food', 'cook food', 'eat the food']
+
+
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/', function (req, res) {
     const today = new Date()
-    const currentDay = today.getDay()
-    let day = ""
 
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday'
-            break;
-        case 1:
-            day = 'Monday'
-            break;
-        case 2:
-            day = 'Tuesday'
-            break;
-        case 3:
-            day = 'Wednesday'
-            break;
-        case 4:
-            day = 'Thursday'
-            break;
-        case 5:
-            day = 'Friday'
-            break;
-        case 6:
-            day = 'Saturday'
-            break;
-        default:
-            console.log(`Error: current day is equal to: ${currentDay}`);
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
     }
 
+    const day = today.toLocaleDateString('en-US', options)
+
     res.render('list', {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     })
 })
 
+app.post('/', function (req, res) {
+    const item = req.body.newItem
+    items.push(item)
+
+    res.redirect('/')
+
+
+})
 
 
 app.listen(process.env.PORT || 3000, function () {
